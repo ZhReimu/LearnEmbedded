c99b5933b5e3a16c6bec31712bf982106a84cef8
博麗靈夢 <1725338233@qq.com>
2022-02-23 16:08:54 +0800

    使用 showBMP 显示 bmp

--- day03/video/color.c
+++ day03/video/color.c
@@ -9,32 +9,26 @@
 void doAction(int lcdfd, char *buff)
 {
 	static int i = 1;
-	//把bmp图片24位颜色数据读取出来
-	char BMP_24bit[BMP_SIZE] = {};
-	char path[] = "./1/01.bmp";
+
+	char path[] = "/1/001.bmp";
 
 	if (i < 10)
 	{
-		path[5] = i + '0';
+		path[6] = i + '0';
+	}
+	else if (i < 100)
+	{
+		path[5] = i / 10 + '0';
+		path[6] = i % 10 + '0';
 	}
 	else
 	{
-		path[4] = i / 10 + '0';
-		path[5] = i % 10 + '0';
-	}
-	if (readBMP(path, BMP_24bit) < 0)
-	{
-		return;
+		path[4] = i / 100 + '0';
+		path[5] = i / 10 % 10 + '0';
+		path[6] = i % 10 + '0';
 	}
 
-	int x, y;
-	for (y = 0; y < 480; y++)
-	{
-		for (x = 0; x < 800; x++)
-		{
-			setBMPColor(buff, BMP_24bit, x, y);
-		}
-	}
+	showBMP(path, 0, 0);
 	usleep(300000);
 	if (i++ == 31)
 		i = 1;
