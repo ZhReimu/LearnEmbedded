cfd6f6e1d40a389ec018e1da1c034770dc8426c3
博麗靈夢 <1725338233@qq.com>
2022-02-24 15:04:01 +0800

    增加 Rect 结构体, 对 矩形区域 进行封装

--- day04/input/color.c
+++ day04/input/color.c
@@ -1,9 +1,11 @@
 #include <myHeader.h>
 
+Rect rect;
+
 void onClick(int x, int y)
 {
 	printf("OnClick in Main : %d, %d\n", x, y);
-	if (inArea(0, 0, 100, 100, x, y))
+	if (inArea2(rect, x, y))
 	{
 		puts("Change Pic");
 		showBMP("./1/001.bmp", 0, 0);
@@ -16,6 +18,11 @@ void onClick(int x, int y)
 }
 int main()
 {
+	rect.startX = 0;
+	rect.startY = 0;
+	rect.endX = 100;
+	rect.endY = 100;
+
 	showBMP("./01.bmp", 0, 0);
 	startTouchThread(onClick);
 	return 0;
