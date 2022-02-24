0a259985804aa31a7f3303c6ad35ae1aa1506a58
博麗靈夢 <1725338233@qq.com>
2022-02-24 17:03:59 +0800

    实现 上一页, 下一页 功能

--- ProjectAlbum/main.c
+++ ProjectAlbum/main.c
@@ -1,28 +1,59 @@
 #include <myHeader.h>
 
-Rect preview;
+Rect prev, next;
 
+void getPath(char *path, int firstZero, int id)
+{
+	if (id < 10)
+	{
+		path[firstZero + 1] = id + '0';
+	}
+	else if (id < 100)
+	{
+		path[firstZero] = id / 10 + '0';
+		path[firstZero + 1] = id % 10 + '0';
+	}
+}
 void onClick(int x, int y)
 {
+	static int i = 1;
+	char path[] = "./pic2/01.bmp";
+	getPath(path, 7, i);
 	debug2D("OnClick in Main : %d, %d\n", x, y, INFO);
-	if (inArea2(preview, x, y))
+	if (inArea2(prev, x, y))
 	{
-		debug("Change Pic", INFO);
-		showBMP("./pic2/02.bmp", 0, 0);
+		if (i++ == 9)
+		{
+			i = 1;
+		}
+		getPath(path, 7, i);
+		debugS("Change Pic To %s", path, INFO);
 	}
 	else
 	{
+		if (i-- == 1)
+		{
+			i = 9;
+		}
+		getPath(path, 7, i);
 		debug("Not Hit", INFO);
-		showBMP("./pic2/01.bmp", 0, 0);
+		debugS("Change Pic To %s", path, INFO);
 	}
+	showBMP(path, 0, 0);
 }
 int main()
 {
-	preview.startX = 0;
-	preview.startY = 500;
+	prev.startX = 0;
+	prev.startY = 512;
 
-	preview.endX = 100;
-	preview.endY = 614;
+	prev.endX = 102;
+	prev.endY = 614;
+
+	next.startX = 102;
+	next.startY = 512;
+
+	next.endX = 204;
+	next.endY = 614;
 
 	showBMP("./pic2/01.bmp", 0, 0);
 	startTouchThread(onClick);
