f59a11875bdbfa36416710d96db5cfc21d4cd4f2
博麗靈夢 <1725338233@qq.com>
2022-02-24 16:20:26 +0800

    更新 printf 为 使用 日志 输出

--- day04/input/color.c
+++ day04/input/color.c
@@ -1,29 +1,30 @@
 #include <myHeader.h>
 
-Rect rect;
+Rect preview;
 
 void onClick(int x, int y)
 {
-	printf("OnClick in Main : %d, %d\n", x, y);
-	if (inArea2(rect, x, y))
+	debug2D("OnClick in Main : %d, %d\n", x, y, INFO);
+	if (inArea2(preview, x, y))
 	{
-		puts("Change Pic");
-		showBMP("./1/001.bmp", 0, 0);
+		debug("Change Pic", INFO);
+		showBMP("./pic2/02.bmp", 0, 0);
 	}
 	else
 	{
-		puts("Not Hit");
-		showBMP("./01.bmp", 0, 0);
+		debug("Not Hit", INFO);
+		showBMP("./pic2/01.bmp", 0, 0);
 	}
 }
 int main()
 {
-	rect.startX = 0;
-	rect.startY = 0;
-	rect.endX = 100;
-	rect.endY = 100;
+	preview.startX = 0;
+	preview.startY = 500;
 
-	showBMP("./01.bmp", 0, 0);
+	preview.endX = 100;
+	preview.endY = 614;
+
+	showBMP("./pic2/01.bmp", 0, 0);
 	startTouchThread(onClick);
 	return 0;
 }
