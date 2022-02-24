a4753edf79c6028b7eb9ae55f4b69253e9dc2ce8
博麗靈夢 <1725338233@qq.com>
2022-02-24 14:41:29 +0800

    增加 点击切换图片 功能

--- day04/input/color.c
+++ day04/input/color.c
@@ -3,9 +3,20 @@
 void onClick(int x, int y)
 {
 	printf("OnClick in Main : %d, %d\n", x, y);
+	if (inArea(0, 0, 100, 100, x, y))
+	{
+		puts("Change Pic");
+		showBMP("./1/001.bmp", 0, 0);
+	}
+	else
+	{
+		puts("Not Hit");
+		showBMP("./01.bmp", 0, 0);
+	}
 }
 int main()
 {
+	showBMP("./01.bmp", 0, 0);
 	startTouchThread(onClick);
 	return 0;
 }
