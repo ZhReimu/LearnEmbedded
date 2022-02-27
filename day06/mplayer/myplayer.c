404c938bd49174b12858858871f1b46c5d646bf8
博麗靈夢 <1725338233@qq.com>
2022-02-27 08:13:51 +0800

    增加注释，修复潜在的问题

--- day06/mplayer/myplayer.c
+++ day06/mplayer/myplayer.c
@@ -24,29 +24,40 @@ Rect btHome;
  * @brief 播放状态 枚举
  * 
  */
-enum SATTUS
+enum STATUS
 {
-	/**
-	 * @brief 暂停中
-	 * 
-	 */
-	PAUSED,
-	/**
-	 * @brief 播放中
-	 * 
-	 */
-	STARTED,
-	/**
-	 * @brief 停止中
-	 * 
-	 */
-	STOPED,
+    /**
+     * @brief 暂停中
+     *
+     */
+    PAUSED,
+    /**
+     * @brief 播放中
+     *
+     */
+    PLAYING,
+    /**
+     * @brief 停止中
+     *
+     */
+    STOPPED,
 };
+/**
+ * @brief 正在播放的 UI
+ * 
+ */
+const char *uiStarting = "/mnt/udisk/2/video-ui-start.bmp";
+/**
+ * @brief 停止中的 UI
+ * 
+ */
+const char *uiStopping = "/mnt/udisk/2/video-ui-stop.bmp";
 /**
  * @brief 当前播放状态
  * 
  */
-static int playStatus = STOPED;
+static int playStatus = STOPPED;
+
 /**
  * @brief 点击事件, 屏幕被点击时触发
  * 
@@ -55,96 +66,113 @@ static int playStatus = STOPED;
  */
 void onClick(int x, int y)
 {
-	static int i = 0;
-	debug2D("Touch Thread Callback OnClick in Main : %d, %d", x, y, INFO);
-	if (inArea2(btPlayAndPause, x, y))
-	{
-		if (playStatus == STOPED)
-		{
-			debug("Play", INFO);
-			playStatus = STARTED;
-			showBMP("/mnt/udisk/2/video-ui-start.bmp", 0, 0, 0);
-			system("mplayer -slave -quiet -input  file=/pipe -zoom -x 800 -y 430 -nosound /mnt/udisk/dream.avi &");
-		}
-		else if (playStatus == STARTED)
-		{
-			showBMP("/mnt/udisk/2/video-ui-stop.bmp", 0, 0, 0);
-			playStatus = PAUSED;
-			system("echo pause >> /pipe");
-			debug("Paused", INFO);
-		}
-		else if (playStatus == PAUSED)
-		{
-			playStatus = STARTED;
-			showBMP("/mnt/udisk/2/video-ui-start.bmp", 0, 0, 0);
-			system("echo pause >> /pipe");
-			debug("Resume", INFO);
-		}
-	}
-	else if (inArea2(btFastForward, x, y))
-	{
-		if (playStatus)
-		{
-			system("echo seek +5 >> /pipe");
-			debug("FastForward", INFO);
-		}
-	}
-	else if (inArea2(btBack, x, y))
-	{
-		if (playStatus)
-		{
-			system("echo seek -5 >> /pipe");
-			debug("Back", INFO);
-		}
-	}
-	else if (inArea2(btHome, x, y))
-	{
-		// fixme: 主页 点击卡死
-		if (playStatus == STARTED || playStatus == PAUSED)
-		{
-			playStatus = STOPED;
-			system("killall -kill mplayer");
-			showBMP("/mnt/udisk/2/video-ui-stop.bmp", 0, 0, 0);
-			debug("Home", INFO);
-		}
-	}
-	else
-	{
-		debug("Not Hit", INFO);
-	}
+    debug2D("Touch Thread Callback OnClick in Main : %d, %d", x, y, INFO);
+    // 如果点击了 播放 & 暂停区域
+    if (inArea2(btPlayAndPause, x, y))
+    {
+        // 如果当前播放状态是 已停止
+        if (playStatus == STOPPED)
+        {
+            // 那就调整标志位, 修改 UI 为 开始播放 状态, 并开始播放
+            playStatus = PLAYING;
+            debug("Start Play", INFO);
+            showBMP(uiStarting, 0, 0, 0);
+            system("mplayer -slave -quiet -input  file=/pipe -zoom -x 800 -y 430 -nosound /mnt/udisk/dream.avi &");
+        }
+        // 如果当前播放状态是 播放中
+        else if (playStatus == PLAYING)
+        {
+            // 那就调整标志位, 修改 UI 为 暂停播放 状态, 并暂停播放
+            playStatus = PAUSED;
+            debug("Paused", INFO);
+            showBMP(uiStopping, 0, 0, 0);
+            system("echo pause >> /pipe");
+        }
+        // 如果当前播放状态是 暂停中
+        else if (playStatus == PAUSED)
+        {
+            // 那就调整标志位, 修改 UI 为 开始播放 状态, 并继续播放
+            playStatus = PLAYING;
+            debug("Resume", INFO);
+            showBMP(uiStarting, 0, 0, 0);
+            system("echo pause >> /pipe");
+        }
+    }
+    // 如果点击了 快进 区域
+    else if (inArea2(btFastForward, x, y))
+    {
+        // 如果当前播放状态是 播放中
+        if (playStatus == PLAYING)
+        {
+            // 那就 快进 5s
+            debug("FastForward", INFO);
+            system("echo seek +5 >> /pipe");
+        }
+    }
+    // 如果点击了 快退 区域
+    else if (inArea2(btBack, x, y))
+    {
+        // 如果当前播放状态是 播放中
+        if (playStatus == PLAYING)
+        {
+            // 那就 快退 5s
+            debug("Back", INFO);
+            system("echo seek -5 >> /pipe");
+        }
+    }
+    // 如果点击了 首页 区域
+    else if (inArea2(btHome, x, y))
+    {
+        // fixme: 主页 点击卡死
+        // 如果当前播放状态是 播放中 或 暂停中
+        if (playStatus == PLAYING || playStatus == PAUSED)
+        {
+            // 那就调整标志位, 结束 mplayer 进程, 修改 UI 为 停止播放 状态
+            playStatus = STOPPED;
+            system("killall -kill mplayer");
+            showBMP(uiStopping, 0, 0, 0);
+            debug("Home", INFO);
+        }
+    }
+    // 如果未 点击有效区域
+    else
+    {
+        debug("Not Hit", INFO);
+    }
 }
+
 void init()
 {
-	btPlayAndPause.startX = 342;
-	btPlayAndPause.startY = 550;
-	btPlayAndPause.endX = 684;
-	btPlayAndPause.endY = 614;
+    btPlayAndPause.startX = 342;
+    btPlayAndPause.startY = 550;
+    btPlayAndPause.endX = 684;
+    btPlayAndPause.endY = 614;
 
-	btFastForward.startX = 684;
-	btFastForward.startY = 550;
-	btFastForward.endX = 960;
-	btFastForward.endY = 614;
+    btFastForward.startX = 684;
+    btFastForward.startY = 550;
+    btFastForward.endX = 960;
+    btFastForward.endY = 614;
 
-	btBack.startX = 0;
-	btBack.startY = 550;
-	btBack.endX = 341;
-	btBack.endY = 614;
+    btBack.startX = 0;
+    btBack.startY = 550;
+    btBack.endX = 341;
+    btBack.endY = 614;
 
-	btHome.startX = 960;
-	btHome.startY = 563;
-	btHome.endX = 1024;
-	btHome.endY = 614;
+    btHome.startX = 960;
+    btHome.startY = 563;
+    btHome.endX = 1024;
+    btHome.endY = 614;
 }
 
 int main()
 {
-	init();
-	// system("mplayer -slave -quiet -input  file=/pipe  -geometry  0:0 -zoom -x 800 -y 480  dream.avi &");
-	// system("echo pause >> /pipe");
-	system("rm /pipe");
-	system("mkfifo /pipe");
-	system("chmod 777 /pipe");
-	showBMP("/mnt/udisk/2/video-ui-stop.bmp", 0, 0, 0);
-	startTouchThread(onClick);
-	return 0;
+    init();
+    // system("mplayer -slave -quiet -input  file=/pipe  -geometry  0:0 -zoom -x 800 -y 480  dream.avi &");
+    // system("echo pause >> /pipe");
+    system("rm /pipe");
+    system("mkfifo /pipe");
+    system("chmod 777 /pipe");
+    showBMP("/mnt/udisk/2/video-ui-stop.bmp", 0, 0, 0);
+    startTouchThread(onClick);
+    return 0;
 }
