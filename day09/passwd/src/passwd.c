438d37f1582622b540665d3dd0f2aa334c4c9cc4
博麗靈夢 <1725338233@qq.com>
2022-03-01 15:39:56 +0800

    新增 读取来自文件的 密码

--- day09/passwd/src/passwd.c
+++ day09/passwd/src/passwd.c
@@ -1,6 +1,16 @@
 #include <passwd.h>
 
 /**
+ * @brief 密码文件 路径
+ * 
+ */
+static const char *pwdFile = "/mnt/udisk/passwd";
+/**
+ * @brief 读取到的 密码内容
+ * 
+ */
+static char pwd[4] = {0};
+/**
  * @brief 播放器处理函数
  * 
  * @param x 点击的 x 坐标
@@ -16,5 +26,13 @@ void passwdHandler(int x, int y)
  */
 void initPasswd()
 {
+    int fd = open(pwdFile, O_RDONLY);
+    if (fd < 0)
+    {
+        debug("Open Password File Failure!", ERROR);
+        exit(-1);
+    }
+    read(fd, pwd, 4);
+    debugS("Correct PWD Is %s", pwd, INFO);
     debug("Init Passwd", INFO);
 }
