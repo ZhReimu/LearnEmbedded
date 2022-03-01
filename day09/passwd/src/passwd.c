ba79c9e8a1728ca6f1265aa3269dbd6427c8549a
博麗靈夢 <1725338233@qq.com>
2022-03-01 17:20:32 +0800

    更新坐标位置

--- day09/passwd/src/passwd.c
+++ day09/passwd/src/passwd.c
@@ -36,6 +36,70 @@ static char pwd[4] = {0};
 void passwdHandler(int x, int y)
 {
     debug2D("PasswdHandler %d, %d", x, y, INFO);
+    if (inArea2(btNum0, x, y))
+    {
+        debug("Hit Num0", INFO);
+    }
+    else if (inArea2(btNum1, x, y))
+    {
+        debug("Hit Num1", INFO);
+    }
+    else if (inArea2(btNum2, x, y))
+    {
+        debug("Hit Num2", INFO);
+    }
+    else if (inArea2(btNum3, x, y))
+    {
+        debug("Hit Num3", INFO);
+    }
+    else if (inArea2(btNum4, x, y))
+    {
+        debug("Hit Num4", INFO);
+    }
+    else if (inArea2(btNum5, x, y))
+    {
+        debug("Hit Num5", INFO);
+    }
+    else if (inArea2(btNum6, x, y))
+    {
+        debug("Hit Num6", INFO);
+    }
+    else if (inArea2(btNum7, x, y))
+    {
+        debug("Hit Num7", INFO);
+    }
+    else if (inArea2(btNum8, x, y))
+    {
+        debug("Hit Num8", INFO);
+    }
+    else if (inArea2(btNum9, x, y))
+    {
+        debug("Hit Num9", INFO);
+    }
+    else if (inArea2(btAccount, x, y))
+    {
+        debug("Hit Account", INFO);
+    }
+    else if (inArea2(btDel, x, y))
+    {
+        debug("Hit Del", INFO);
+    }
+    else if (inArea2(btPasswd, x, y))
+    {
+        debug("Hit Passwd", INFO);
+    }
+    else if (inArea2(btShowPass, x, y))
+    {
+        debug("Hit ShowPass", INFO);
+    }
+    else if (inArea2(btYes, x, y))
+    {
+        debug("Hit Yes", INFO);
+    }
+    else
+    {
+        debug("Not Hit", INFO);
+    }
 }
 /**
  * @brief 初始化 点击区域
@@ -53,80 +117,80 @@ void initPasswd()
     debugS("Correct PWD Is %s", pwd, INFO);
     showBMPOO(bg);
 
-    btNum0.startX = 697;
-    btNum0.endX = 762;
-    btNum0.startY = 302;
-    btNum0.endY = 395;
+    btNum0.startX = 892;
+    btNum0.endX = 975;
+    btNum0.startY = 378;
+    btNum0.endY = 506;
 
-    btNum1.startX = 502;
-    btNum1.endX = 567;
-    btNum1.startY = 117;
-    btNum1.endY = 210;
+    btNum1.startX = 643;
+    btNum1.endX = 726;
+    btNum1.startY = 150;
+    btNum1.endY = 267;
 
-    btNum2.startX = 567;
-    btNum2.endX = 632;
-    btNum2.startY = 117;
-    btNum2.endY = 210;
+    btNum2.startX = 726;
+    btNum2.endX = 809;
+    btNum2.startY = 150;
+    btNum2.endY = 267;
 
-    btNum3.startX = 632;
-    btNum3.endX = 697;
-    btNum3.startY = 117;
-    btNum3.endY = 210;
+    btNum3.startX = 809;
+    btNum3.endX = 892;
+    btNum3.startY = 150;
+    btNum3.endY = 267;
 
-    btNum4.startX = 502;
-    btNum4.endX = 567;
-    btNum4.startY = 210;
-    btNum4.endY = 302;
+    btNum4.startX = 643;
+    btNum4.endX = 726;
+    btNum4.startY = 267;
+    btNum4.endY = 387;
 
-    btNum5.startX = 567;
-    btNum5.endX = 632;
-    btNum5.startY = 210;
-    btNum5.endY = 302;
+    btNum5.startX = 726;
+    btNum5.endX = 809;
+    btNum5.startY = 267;
+    btNum5.endY = 387;
 
-    btNum6.startX = 632;
-    btNum6.endX = 697;
-    btNum6.startY = 210;
-    btNum6.endY = 302;
+    btNum6.startX = 809;
+    btNum6.endX = 892;
+    btNum6.startY = 267;
+    btNum6.endY = 387;
 
-    btNum7.startX = 502;
-    btNum7.endX = 567;
-    btNum7.startY = 302;
-    btNum7.endY = 395;
+    btNum7.startX = 643;
+    btNum7.endX = 726;
+    btNum7.startY = 378;
+    btNum7.endY = 506;
 
-    btNum8.startX = 567;
-    btNum8.endX = 632;
-    btNum8.startY = 302;
-    btNum8.endY = 395;
+    btNum8.startX = 726;
+    btNum8.endX = 809;
+    btNum8.startY = 378;
+    btNum8.endY = 506;
 
-    btNum9.startX = 632;
-    btNum9.endX = 697;
-    btNum9.startY = 302;
-    btNum9.endY = 395;
+    btNum9.startX = 809;
+    btNum9.endX = 892;
+    btNum9.startY = 378;
+    btNum9.endY = 506;
 
-    btDel.startX = 697;
-    btDel.endX = 762;
-    btDel.startY = 117;
-    btDel.endY = 210;
+    btDel.startX = 892;
+    btDel.endX = 975;
+    btDel.startY = 150;
+    btDel.endY = 267;
 
-    btYes.startX = 697;
-    btYes.endX = 762;
-    btYes.startY = 210;
-    btYes.endY = 302;
+    btYes.startX = 892;
+    btYes.endX = 975;
+    btYes.startY = 269;
+    btYes.endY = 387;
 
-    btAccount.startX = 94;
-    btAccount.endX = 480;
-    btAccount.startY = 117;
-    btAccount.endY = 209;
+    btAccount.startX = 120;
+    btAccount.endX = 614;
+    btAccount.startY = 150;
+    btAccount.endY = 268;
 
-    btPasswd.startX = 94;
-    btPasswd.endX = 480;
-    btPasswd.startY = 209;
-    btPasswd.endY = 301;
+    btPasswd.startX = 120;
+    btPasswd.endX = 614;
+    btPasswd.startY = 268;
+    btPasswd.endY = 385;
 
-    btShowPass.startX = 95;
-    btShowPass.endX = 480;
-    btShowPass.startY = 303;
-    btShowPass.endY = 395;
+    btShowPass.startX = 122;
+    btShowPass.endX = 614;
+    btShowPass.startY = 388;
+    btShowPass.endY = 506;
 
     debug("Init Passwd", INFO);
 }
