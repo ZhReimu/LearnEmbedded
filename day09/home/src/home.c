c0ddb919d3430551f9cbb948ae61b707955b3050
博麗靈夢 <1725338233@qq.com>
2022-03-02 15:57:38 +0800

    初始化 home 模块

---
+++ day09/home/src/home.c
@@ -0,0 +1,9 @@
+#include <home.h>
+
+void homeHandler(int x, int y)
+{
+}
+
+void initHome()
+{
+}
---
+++ day09/home/src/main.c
@@ -0,0 +1,18 @@
+#include <home.h>
+
+/**
+ * @brief 点击事件, 屏幕被点击时触发
+ * 
+ * @param x 被点击的 x 坐标
+ * @param y 被点击的 y 坐标
+ */
+void onClick(int x, int y)
+{
+	homeHandler(x, y);
+}
+int main()
+{
+	initHome();
+	startTouchThread(onClick);
+	return 0;
+}
