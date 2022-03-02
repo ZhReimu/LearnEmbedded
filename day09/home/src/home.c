015497182b5218be82bc7e421e2be14039cf91aa
博麗靈夢 <1725338233@qq.com>
2022-03-02 16:04:44 +0800

    增加 大首页 按钮

--- day09/home/src/home.c
+++ day09/home/src/home.c
@@ -1,9 +1,48 @@
 #include <home.h>
 
+Rect btThumAlbum;
+Rect btAlbum;
+Rect btThumVideoPlayer;
+Rect btVideoPlayer;
+Rect btThumMusicPlayer;
+Rect btMusicPlayer;
+
+const char *home = "/mnt/udisk/ProjectAlbum/home.bmp";
 void homeHandler(int x, int y)
 {
 }
 
 void initHome()
 {
+    btThumAlbum.startX;
+    btThumAlbum.startY;
+    btThumAlbum.endX;
+    btThumAlbum.endY;
+
+    btAlbum.startX;
+    btAlbum.startY;
+    btAlbum.endX;
+    btAlbum.endY;
+
+    btThumVideoPlayer.startX;
+    btThumVideoPlayer.startY;
+    btThumVideoPlayer.endX;
+    btThumVideoPlayer.endY;
+
+    btVideoPlayer.startX;
+    btVideoPlayer.startY;
+    btVideoPlayer.endX;
+    btVideoPlayer.endY;
+
+    btThumMusicPlayer.startX;
+    btThumMusicPlayer.startY;
+    btThumMusicPlayer.endX;
+    btThumMusicPlayer.endY;
+
+    btMusicPlayer.startX;
+    btMusicPlayer.startY;
+    btMusicPlayer.endX;
+    btMusicPlayer.endY;
+
+    showBMPOO(home);
 }
