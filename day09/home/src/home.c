14648d04973110ae5d4672c14303be18bcfc4d3a
博麗靈夢 <1725338233@qq.com>
2022-03-02 16:12:02 +0800

    补全坐标

--- day09/home/src/home.c
+++ day09/home/src/home.c
@@ -6,6 +6,7 @@ Rect btThumVideoPlayer;
 Rect btVideoPlayer;
 Rect btThumMusicPlayer;
 Rect btMusicPlayer;
+Rect btLockScreen;
 
 const char *home = "/mnt/udisk/ProjectAlbum/home.bmp";
 void homeHandler(int x, int y)
@@ -14,35 +15,40 @@ void homeHandler(int x, int y)
 
 void initHome()
 {
-    btThumAlbum.startX;
-    btThumAlbum.startY;
-    btThumAlbum.endX;
-    btThumAlbum.endY;
+    btThumAlbum.startX = 64;
+    btThumAlbum.startY = 156;
+    btThumAlbum.endX = 320;
+    btThumAlbum.endY = 216;
 
-    btAlbum.startX;
-    btAlbum.startY;
-    btAlbum.endX;
-    btAlbum.endY;
+    btAlbum.startX = 198;
+    btAlbum.startY = 505;
+    btAlbum.endX = 262;
+    btAlbum.endY = 569;
 
-    btThumVideoPlayer.startX;
-    btThumVideoPlayer.startY;
-    btThumVideoPlayer.endX;
-    btThumVideoPlayer.endY;
+    btThumVideoPlayer.startX = 385;
+    btThumVideoPlayer.startY = 156;
+    btThumVideoPlayer.endX = 641;
+    btThumVideoPlayer.endY = 216;
 
-    btVideoPlayer.startX;
-    btVideoPlayer.startY;
-    btVideoPlayer.endX;
-    btVideoPlayer.endY;
+    btVideoPlayer.startX = 480;
+    btVideoPlayer.startY = 505;
+    btVideoPlayer.endX = 544;
+    btVideoPlayer.endY = 569;
 
-    btThumMusicPlayer.startX;
-    btThumMusicPlayer.startY;
-    btThumMusicPlayer.endX;
-    btThumMusicPlayer.endY;
+    btThumMusicPlayer.startX = 704;
+    btThumMusicPlayer.startY = 156;
+    btThumMusicPlayer.endX = 960;
+    btThumMusicPlayer.endY = 216;
 
-    btMusicPlayer.startX;
-    btMusicPlayer.startY;
-    btMusicPlayer.endX;
-    btMusicPlayer.endY;
+    btMusicPlayer.startX = 761;
+    btMusicPlayer.startY = 505;
+    btMusicPlayer.endX = 825;
+    btMusicPlayer.endY = 569;
+
+    btLockScreen.startX = 972;
+    btLockScreen.startY = 569;
+    btLockScreen.endX = 1024;
+    btLockScreen.endY = 614;
 
     showBMPOO(home);
 }
