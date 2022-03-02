60a9ca5391ad5e5fa3bcf17fb8bcd700a98a0196
博麗靈夢 <1725338233@qq.com>
2022-03-02 18:37:11 +0800

    增加注释

--- MyAlbum/include/global.h
+++ MyAlbum/include/global.h
@@ -1,12 +1,36 @@
 #ifndef _GLOBAL_H
 #define _GLOBAL_H
 
+/**
+ * @brief 模块 枚举
+ * 
+ */
 enum MODULE
 {
+    /**
+     * @brief 密码 模块
+     * 
+     */
     PASSWORD,
+    /**
+     * @brief 主页 模块
+     * 
+     */
     HOME,
+    /**
+     * @brief 相册 模块
+     * 
+     */
     ALBUM,
+    /**
+     * @brief 视频播放器 模块
+     * 
+     */
     VPLAYER,
+    /**
+     * @brief 音乐播放器 模块
+     * 
+     */
     MPLAYER
 };
 /**
